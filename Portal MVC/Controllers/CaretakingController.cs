358c61e79c97afbb28da3cdaf1e270d9057a104c
=Adam New <adam.new@newestates.co.uk>
2021-01-03 11:26:42 +0000

    Attendance Visists

--- Portal MVC/Controllers/CaretakingController.cs
+++ Portal MVC/Controllers/CaretakingController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -17,9 +18,46 @@ namespace Portal_MVC.Controllers
         public ActionResult AttendanceLog()
         {
             Models.AttendanceLogViewModel vm = new Models.AttendanceLogViewModel();
-            vm.EstateList = Models.PropertyMethods.GetAllEstates();
+            vm.SetLists();
 
             return View(vm);
+        }
+
+        [HttpPost]
+        public ActionResult SubmitAttendance(Models.AttendanceLogViewModel ViewModel)
+        {
+            var httpPostedFile = System.Web.HttpContext.Current.Request.Files["UploadFiles"];
+            var fileSave = System.Web.HttpContext.Current.Server.MapPath("UploadedFiles");
+            var fileSavePath = Path.Combine(fileSave, httpPostedFile.FileName);
+
+            byte[] fileData = null;
+            using (var binaryReader = new BinaryReader(Request.Files[0].InputStream))
+            {
+                fileData = binaryReader.ReadBytes(Request.Files[0].ContentLength);
+            }
+            
+            foreach (var file in Request.Files)
+            {
+                if (file != null)
+                {
+                    using (var ms = new MemoryStream())
+                    {
+                        file.CopyTo(ms);
+                        var fileBytes = ms.ToArray();
+                        string s = Convert.ToBase64String(fileBytes);
+                        // act on the Base64 data
+                    }
+                }
+            }
+
+            if (ModelState.IsValid)
+            {
+
+            } else
+            {
+                ViewModel.SetLists();
+            }
+            return View("AttendanceLog", ViewModel);
         }
     }
 }
--- Portal MVC/Models/AttendanceLogViewModel.cs
+++ Portal MVC/Models/AttendanceLogViewModel.cs
@@ -3,17 +3,74 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.ComponentModel.DataAnnotations;
-
+using System.Web.Mvc;
 
 namespace Portal_MVC.Models
 {
     public class AttendanceLogViewModel
     {
-        public IEnumerable<Properties> EstateList { get; set; }
+        public AttendanceLogViewModel()
+        {
+            AttendanceObj = new AttendanceVisits.AttendanceVisits();
+            
+        }
+        public List<SelectListItem> EstateList { get; set; }
+        public int SelectedPropertyid { get; set; }
+        public List<SelectListItem> AttendanceTypes { get; set; }
+        public int SelectedAttendanceTypeID { get; set; }
+        public AttendanceVisits.AttendanceVisits AttendanceObj { get; set; }
 
+        [Required(ErrorMessage = "Description of the Attendance is required.")]
+        public string AttendanceDescription { get; set; }
         
         public bool CaretakingVisit { get; set; }
         public bool InspectionVisit { get; set; }
         public bool CallOutVisit { get; set; }
+
+       
+
+        public void SetLists()
+        {
+            List<Properties> estates = Models.PropertyMethods.GetAllEstates();
+            EstateList = new List<SelectListItem>();
+            foreach (Properties p in estates)
+            {
+                bool IsSelected = false;
+                if(p.ID == SelectedPropertyid && SelectedPropertyid > 0)
+                {
+                    IsSelected = true;
+                }
+
+                EstateList.Add(new SelectListItem
+                {
+                    Text = p.Address1,
+                    Value = p.ID.ToString(),
+                    Selected = IsSelected
+                });
+            }
+
+            List<AttendanceVisits.AttendanceType> types = 
+                AttendanceVisits.AttendanceTypeMethods.AllAttendanceTypesList(GlobalVariables.CS);
+            AttendanceTypes = new List<SelectListItem>();
+
+            foreach(AttendanceVisits.AttendanceType t in types)
+            {
+                bool IsSelected = false;
+                if (t.id == SelectedAttendanceTypeID && SelectedAttendanceTypeID > 0)
+                {
+                    IsSelected = true;
+                }
+
+                AttendanceTypes.Add(new SelectListItem
+                {
+                    Text = t.Name,
+                    Value = t.id.ToString(),
+                    Selected = IsSelected
+                });
+            }
+
+        }
+
+
     }
 }
